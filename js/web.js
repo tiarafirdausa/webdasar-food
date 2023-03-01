aa50f1bacf79b2134bc8fb746f7118bd2de143a5
Tiara Firdausa <tiarafirdausa94@gmail.com>
2023-03-02 02:04:39 +0700

    first commit

---
+++ js/web.js
@@ -0,0 +1,16 @@
+// toggle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.addEventListener('click', function (e) {
+    if (hamburger.contains(e.target)) {
+        navbarNav.classList.toggle('active');
+    }
+});
+
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function (e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+});
